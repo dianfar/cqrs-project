c06cafa2c877ae430e8b172eee34b2e363aba3de
samuelalvin <samuel.alvin1994@gmail.com>
2017-10-09 18:31:36 +0700

    add `addClient` and `editClient` pages on `MyApp.WebApi` project

--- MyApp.WebApi/Frontend/src/client/store.ts
+++ MyApp.WebApi/Frontend/src/client/store.ts
@@ -3,22 +3,41 @@ import * as fetch from "isomorphic-fetch";
 import { IClient } from "./interface";
 
 export class ClientStore {
-    @observable clients: IClient[];
-
-    constructor(clients?: IClient[]) {
-        if (clients) {
-            this.clients = clients;
-            return;
-        }
-
-        this.clients = [];
-    }
-
+    @observable clients: IClient[] = [];
+    
     async getClients(): Promise<IClient[]> {
         let response = await fetch("/api/clients");
-        let clients = await response.json() as IObservableArray<IClient>;
+        let clients = await response.json() as IClient[];
         this.clients = clients;
         return this.clients;
+    }
+
+    async getClient(clientId: string): Promise<IClient> {
+        let response = await fetch(`/api/clients/${clientId}`);
+        let client = await response.json() as IClient;
+        return client;
+    }
+
+    async updateClient(client: IClient): Promise<void> {
+        await fetch(`/api/clients/`, {
+            method: "put",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(client)
+        });
+    }
+
+    async addClient(client: IClient): Promise<void> {
+        await fetch(`/api/clients/`, {
+            method: "post",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(client)
+        });
     }
 
     async deleteClient(clientId: string): Promise<void> {

e74b218f898c8428b75ebb8d2118a53c1efa3817
samuelalvin <samuel.alvin1994@gmail.com>
2017-10-11 17:02:31 +0700

    use axios instead of isomorphic-fetch because it doesn't need boilerplate code

--- MyApp.WebApi/Frontend/src/client/store.ts
+++ MyApp.WebApi/Frontend/src/client/store.ts
@@ -1,46 +1,30 @@
 ﻿import { IObservableArray, observable } from "mobx";
-import * as fetch from "isomorphic-fetch";
+import axios from "axios";
 import { IClient } from "./interface";
 
 export class ClientStore {
     async getClients(): Promise<IClient[]> {
-        let response = await fetch("/api/clients");
-        let clients = await response.json() as IClient[];
+        let response = await axios.get("/api/clients");
+        let clients = response.data as IClient[];
         return clients;
     }
 
     async getClient(clientId: string): Promise<IClient> {
-        let response = await fetch(`/api/clients/${clientId}`);
-        let client = await response.json() as IClient;
+        let response = await axios.get(`/api/clients/${clientId}`);
+        let client = response.data as IClient;
         return client;
     }
 
     async updateClient(client: IClient): Promise<void> {
-        await fetch(`/api/clients/`, {
-            method: "put",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(client)
-        });
+        await axios.put(`/api/clients/`, client);
     }
 
     async addClient(client: IClient): Promise<void> {
-        await fetch(`/api/clients/`, {
-            method: "post",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(client)
-        });
+        await axios.post(`/api/clients/`, client);
     }
 
     async deleteClient(clientId: string): Promise<void> {
-        let response = await fetch(`/api/clients/${clientId}`, {
-            method: "delete"
-        });
+        await axios.delete(`/api/clients/${clientId}`);
     }
 }
 
